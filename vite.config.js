1a29db4771b65e2a9e202d15cab94b015af1f0f4
simmuuu <113759012+simmuuu@users.noreply.github.com>
2025-07-07 11:28:21 +0530

    docker fix3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',     // ← Accept connections from any IP
+    allowedHosts: true,     // ← Accept connections from any IP
     port: 3001,
     strictPort: true,    // ← Fail fast if port is taken (optional)
     open: false,         // ← Don't auto-open browser
